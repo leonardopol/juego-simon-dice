ecb6a10277126e4e711d411d437d20be52eb560f
leonardopol <leonardo_pol@hotmail.com>
2023-08-07 15:56:32 -0300

    se arreglan nombres de funciones en main.js
    para mejor legibilidad y tambien se agrega funcion de cambio de color de alerta al reinicio del juego

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let guardarMovimientosMaquina = [];
-let guardarMovimientosJugador = [];
+let movimientosMaquina = [];
+let movimientosJugador = [];
 let ronda = 0;
 let indice = 0;
 let estado = '';
@@ -10,31 +10,32 @@ function empezar(){
     reiniciarMovimientos();
     ronda = 1;
     indice = 0;
-    actualizaRonda();
-    actualizaEstado(`Turno computadora`);
+    actualizarRonda();
+    actualizarEstado(`Turno computadora`);
+    actualizarColorAlertaInicioDeJuego();
     turnoComputadora();
 }
 
 function turnoComputadora(){
     
     bloquearUsuario();
-    guardarMovimientosMaquina.push(obtenerCuadro());
-    const RETRASO_TURNO_JUGADOR = (guardarMovimientosMaquina.length + 1) * 1000;
+    movimientosMaquina.push(obtenerCuadroAleatoreo());
+    const RETRASO_TURNO_JUGADOR = (movimientosMaquina.length + 1) * 1000;
 
-    guardarMovimientosMaquina.forEach(function($cuadro, index){
+    movimientosMaquina.forEach(function($cuadro, index){
         const RETRASO_MS = (index + 1) * 1000;
         setTimeout(function() {
-            resaltar($cuadro);
+            resaltarCuadro($cuadro);
         },RETRASO_MS);
     });
 
     setTimeout(function() {
-        actualizaEstado('Turno del jugador');
+        actualizarEstado('Turno del jugador');
         desbloquearUsuario();
       }, RETRASO_TURNO_JUGADOR);
 }
 
-function obtenerCuadro(){
+function obtenerCuadroAleatoreo(){
     const $cuadros = document.querySelectorAll(".cuadro");
     const cuadro = $cuadros[Math.floor(Math.random() * $cuadros.length)];
     return cuadro;    
@@ -50,20 +51,20 @@ function entradaJugador(e){
 
     const $cuadro = e.target;
     indice++;
-    resaltar($cuadro);
+    resaltarCuadro($cuadro);
     
-    if($cuadro !== guardarMovimientosMaquina[indice - 1]){
-        actualizaEstado(`Perdisteee! toca "Empezar" para volver a Jugar!`);
-        estadoFinDeJuego();
+    if($cuadro !== movimientosMaquina[indice - 1]){
+        actualizarEstado(`Perdisteee! toca "Empezar" para volver a Jugar!`);
+        actualizarColorAlertaFinDeJuego();
         ronda = 0;
-        actualizaRonda();
+        actualizarRonda();
         bloquearUsuario();
         return;
     }
 
-    guardarMovimientosJugador.push($cuadro);
+    movimientosJugador.push($cuadro);
 
-    if(guardarMovimientosJugador.length === guardarMovimientosMaquina.length){
+    if(movimientosJugador.length === movimientosMaquina.length){
         compararJugada();
     }
     
@@ -71,10 +72,10 @@ function entradaJugador(e){
 
 function compararJugada(){
     
-    for(let i = 0; i < guardarMovimientosMaquina.length; i++){
-        if(guardarMovimientosMaquina[i] === guardarMovimientosJugador[i]){
+    for(let i = 0; i < movimientosMaquina.length; i++){
+        if(movimientosMaquina[i] === movimientosJugador[i]){
             setTimeout(function(){
-                actualizaEstado(`Turno computadora`);
+                actualizarEstado(`Turno computadora`);
                 
             },1000);
 
@@ -84,7 +85,7 @@ function compararJugada(){
     setTimeout(function(){
 
         ronda++;
-        actualizaRonda();
+        actualizarRonda();
         turnoComputadora();
         
     },1000);
@@ -93,12 +94,12 @@ function compararJugada(){
 }
 
 function reiniciarMovimientosJugador(){
-    guardarMovimientosJugador = [];
+    movimientosJugador = [];
 }
 
 function reiniciarMovimientos(){
-    guardarMovimientosMaquina = [];
-    guardarMovimientosJugador = [];
+    movimientosMaquina = [];
+    movimientosJugador = [];
 }
 
 function bloquearUsuario(){
@@ -114,22 +115,27 @@ function desbloquearUsuario() {
     });
 }
 
-function actualizaRonda(){
+function actualizarRonda(){
     const $ronda = document.querySelector("#ronda");
     $ronda.textContent = ronda;
 }
 
-function actualizaEstado(estado){
+function actualizarEstado(estado){
     const $estado = document.querySelector("#estado");
     $estado.textContent = estado;
 }
 
-function estadoFinDeJuego(){
+function actualizarColorAlertaFinDeJuego(){
     const $alerta = document.querySelector("#estado");
     $alerta.style.color = "red";
 }
 
-function resaltar($cuadro) {
+function actualizarColorAlertaInicioDeJuego(){
+    const $alerta = document.querySelector("#estado");
+    $alerta.style.color = "green";
+}
+
+function resaltarCuadro($cuadro) {
     $cuadro.style.opacity = 1;
     setTimeout(function() {
       $cuadro.style.opacity = 0.5;
